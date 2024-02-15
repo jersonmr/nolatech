63fd419971144786d393d091fa9ce83970403980
Jerson Moreno <jersonmr@gmail.com>
2024-02-15 17:09:45 -0400

    Add update user in the dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,9 +1,49 @@
+import { check, validationResult } from "express-validator";
+import User from "../models/User.js";
+
 const dashboard = async (req, res) => {
+    const success = req.query.success;
     res.render("app/dashboard", {
         title: "Dashboard",
         csrfToken: req.csrfToken(),
-        username: req.user.username,
+        user: req.user,
+        success,
     });
 };
 
-export { dashboard };
+const updateUser = async (req, res) => {
+    const { name, surname, username, email } = req.body;
+
+    await check("name", "Nombre es requerido").notEmpty().run(req);
+    await check("surname", "Apellido es requerido").notEmpty().run(req);
+    await check("username", "Nombre de usuario es requerido")
+        .notEmpty()
+        .run(req);
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.render("app/dashboard", {
+            title: "Dashboard",
+            csrfToken: req.csrfToken(),
+            user: req.user,
+            errors: errors.array(),
+        });
+    }
+
+    const user = await User.findOne({
+        where: { email: req.user.email },
+    });
+
+    user.name = name;
+    user.surname = surname;
+    user.username = username;
+
+    await user.save();
+
+    res.redirect(
+        "/dashboard?success=Los datos se han actualizado correctamente."
+    );
+};
+
+export { dashboard, updateUser };
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { dashboard } from "../controllers/dashboardController.js";
+import { dashboard, updateUser } from "../controllers/dashboardController.js";
 
 const router = express.Router();
 
 router.get("/", authMiddleware, dashboard);
+
+router.post("/update-user", authMiddleware, updateUser);
 
 export default router;
